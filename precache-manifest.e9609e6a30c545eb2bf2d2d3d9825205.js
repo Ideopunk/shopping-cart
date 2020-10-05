454c65acdaf9811703925c3d4309b0d75e80ac39
Conor Barnes <conorbarnes93@gmail.com>
2020-10-05 16:03:57 -0700

    Updates

---
+++ precache-manifest.e9609e6a30c545eb2bf2d2d3d9825205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d34df29fa71ce6d49b6467dbda11f3c",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "1ee2be4e9abd012989cb",
+    "url": "/shopping-cart/static/css/main.bb00d67e.chunk.css"
+  },
+  {
+    "revision": "a4a0389e13cbc5d74a4f",
+    "url": "/shopping-cart/static/js/2.3e6d9649.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.3e6d9649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee2be4e9abd012989cb",
+    "url": "/shopping-cart/static/js/main.defcbc58.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
