caf42046f5724339b950de5edd02eaaf9da89d0f
Conor Barnes <conorbarnes93@gmail.com>
2020-10-05 10:19:31 -0700

    Initialize basic components for routing, basic css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './styles/App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-yo
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/shop">Shop</Link>
+        </nav>
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About} />
+            <Route exact path="/shop" component={Shop} />
+          </Switch>
+        </div>
+      </div>  
+    </Router>
   );
+}
+
+const Home = () => {
+  return (<div>home</div>)
+}
+
+const About = () => {
+  return (<div>About</div>)
+}
+
+const Shop = () => {
+  return (<div>Shop</div>)
 }
 
 export default App;
