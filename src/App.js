d02a2d64f7d51b888fa19cb14d011d4c8e33a997
Conor Barnes <conorbarnes93@gmail.com>
2020-10-05 22:13:45 -0700

    switch to hash-router for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
 import "./styles/App.scss";
 import Shop from "./components/Shop";
 
